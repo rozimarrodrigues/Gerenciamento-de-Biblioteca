ed959a5e35be41496ae76999442eb6b97467244e
Emanoel Rair Amancio Lima <rair.lima@DCE-L2C2.(none)>
2013-07-01 13:51:39 -0300

    alterando Aluno.java

--- src/br/ufpb/poo/projeto/Aluno.java
+++ src/br/ufpb/poo/projeto/Aluno.java
@@ -1,9 +1,15 @@
 package br.ufpb.poo.projeto;
 
 public class Aluno {
-
+	private string nome;
 	public void setMatricula(String string) {
 		// TODO Auto-generated method stub
+		public void setNome(String nome){
+			this.nome=nome;
+		}
+		public String getNome(){
+			return nome;
+		}
 		
 	}
 
