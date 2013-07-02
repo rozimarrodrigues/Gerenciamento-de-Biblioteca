744a16256e8d435a24351895e196b312b4186781
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-07-02 11:18:40 -0300

    Corrigindo erros na classe Aluno

--- src/br/ufpb/poo/projeto/Aluno.java
+++ src/br/ufpb/poo/projeto/Aluno.java
@@ -1,16 +1,17 @@
 package br.ufpb.poo.projeto;
 
 public class Aluno {
-	private string nome;
+	private String nome;
+
 	public void setMatricula(String string) {
 		// TODO Auto-generated method stub
-		public void setNome(String nome){
-			this.nome=nome;
-		}
-		public String getNome(){
-			return nome;
-		}
-		
 	}
-
+	
+	public void setNome(String nome){
+		this.nome=nome;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
 }
