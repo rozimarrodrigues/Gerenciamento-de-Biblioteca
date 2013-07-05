0b0f3ca106bf77eb08a965030c677acecadf1c80
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-07-05 08:42:48 -0300

    Criação do atributo curso na classe Aluno e gets e sets

--- src/br/ufpb/poo/projeto/Aluno.java
+++ src/br/ufpb/poo/projeto/Aluno.java
@@ -2,11 +2,19 @@ package br.ufpb.poo.projeto;
 
 public class Aluno extends Pessoa {
 	private int matricula;
+	private String curso;
 
 	public int getMatricula(){
 		return matricula;
 	}
 	public void setMatricula(int matricula) {
 		this.matricula = matricula;
+	}
+	
+	public String getCurso(){
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
 	}
 }
