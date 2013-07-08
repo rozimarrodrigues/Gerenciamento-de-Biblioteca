28affbd7600dab526dae473b53ca1bf79171285c
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-07-08 02:29:58 -0300

    Implementação dos Gets e Sets na classe Funcionario

--- src/br/ufpb/poo/projeto/Funcionario.java
+++ src/br/ufpb/poo/projeto/Funcionario.java
@@ -5,11 +5,17 @@ public class Funcionario extends Pessoa {
 	private int matricula;
 	private String funcao;
 	
+	public int getMatricula(){
+		return matricula;
+	}
 	public void setMatricula(int matricula) {
-		
+		this.matricula = matricula;
 	}
 
-	public void setFuncao(String string) {
-		
+	public String getFuncao(){
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
 	}
 }
