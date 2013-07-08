8639a5f714ffd31bb7538e0a38305b22842f9286
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-07-08 02:31:42 -0300

    Criação da lista de funcionarios na classe GerenteFuncionario

--- src/br/ufpb/poo/projeto/GerenteFuncionario.java
+++ src/br/ufpb/poo/projeto/GerenteFuncionario.java
@@ -1,7 +1,12 @@
 package br.ufpb.poo.projeto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenteFuncionario {
 
+	private List<Funcionario> funcionarios = new ArrayList<Funcionario>();
+	
 	public void addFuncionario(Funcionario funcionario) {
 		
 	}
