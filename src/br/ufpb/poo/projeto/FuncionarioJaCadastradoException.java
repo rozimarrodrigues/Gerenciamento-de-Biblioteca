9e49cbe20b8b73a40d75f01663025a5e3469356a
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-07-08 02:42:04 -0300

    Criação da exceção FuncionarioJaCadastradoException

---
+++ src/br/ufpb/poo/projeto/FuncionarioJaCadastradoException.java
@@ -0,0 +1,8 @@
+package br.ufpb.poo.projeto;
+
+@SuppressWarnings("serial")
+public class FuncionarioJaCadastradoException extends RuntimeException {
+	public FuncionarioJaCadastradoException(String msg){
+		super(msg);
+	}
+}
