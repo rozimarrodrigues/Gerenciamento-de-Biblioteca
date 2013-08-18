fe6221fbcebd51547cbcdda95f8d05b024d67b10
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-08-18 19:27:25 -0300

    Criação das lista professores

--- src/br/ufpb/poo/projeto/GerenteProfessor.java
+++ src/br/ufpb/poo/projeto/GerenteProfessor.java
@@ -1,9 +1,13 @@
 package br.ufpb.poo.projeto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenteProfessor {
+	
+	private List<Professor> professores = new ArrayList<Professor>(); 
 
 	public void addProfessor(Professor professor) {
-		
 		
 	}
 
