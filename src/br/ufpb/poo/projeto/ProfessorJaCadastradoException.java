424692b1815595c15f7d579a93491913c3a9cdfe
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-08-18 19:57:59 -0300

    Criação da exceção ProfessorJaCadastradoException

---
+++ src/br/ufpb/poo/projeto/ProfessorJaCadastradoException.java
@@ -0,0 +1,9 @@
+package br.ufpb.poo.projeto;
+
+@SuppressWarnings("serial")
+public class ProfessorJaCadastradoException extends RuntimeException {
+	
+	public ProfessorJaCadastradoException(String msg){
+		super(msg);
+	}
+}
