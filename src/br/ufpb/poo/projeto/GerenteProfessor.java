9cc1f39c5525ea07d1293fb74c665abb3f71f48c
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-08-18 22:25:20 -0300

    Crição do metodo professorJaCadastrado para fazer o teste passar

--- src/br/ufpb/poo/projeto/GerenteProfessor.java
+++ src/br/ufpb/poo/projeto/GerenteProfessor.java
@@ -8,11 +8,24 @@ public class GerenteProfessor {
 	private List<Professor> professores = new ArrayList<Professor>(); 
 
 	public void addProfessor(Professor professor) {
+		if(professorJaCadastrado(professor)){
+			throw new ProfessorJaCadastradoException("Professor ja cadastrado!!!");
+		}
 		professores.add(professor);
 	}
 
 	public Professor getProfessor(int pos){
 		return professores.get(pos);
+	}
+	
+	public boolean professorJaCadastrado(Pessoa professor){
+		boolean existe = false;
+		for(Pessoa a: professores){
+			if(a.equals(professor)){
+				existe = true;
+			}
+		}
+		return existe;
 	}
 
 }
