16d966b4323a7ae5c24496a30dafdccdc7d9084a
Rozimar Rodrigues <rrozimar@hotmail.com>
2013-09-17 14:50:05 -0300

    Alteração na exceção LivroJaCadastradoException

--- src/br/ufpb/poo/biblioteca/excecao/LivroJaCadastradoException.java
+++ src/br/ufpb/poo/biblioteca/excecao/LivroJaCadastradoException.java
@@ -1,6 +1,6 @@
 package br.ufpb.poo.biblioteca.excecao;
 
-public class LivroJaCadastradoException extends Exception {
+public class LivroJaCadastradoException extends RuntimeException {
 
 	public LivroJaCadastradoException(String msg){
 		super();
